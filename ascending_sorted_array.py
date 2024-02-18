f529b6b5fdb43ed8a0e8249fb669187955bc607b
preetamprabhu <preetam11prabhu@gmail.com>
2024-02-18 17:41:04 +0530

    check ascending nature

---
+++ ascending_sorted_array.py
@@ -0,0 +1,8 @@
+class  Check_Sorted_Array:
+    def isSorted(n: int,  a: [int]) -> int:
+        for i in range(1,n):
+            if (a[i] < a[i - 1]):
+                return 0 
+            
+        return 1
+
